575e41f454527cec2921d441398fee580a9050c3
tien-tq200540 <tqtien157202@gmail.com>
2025-01-25 23:25:35 +0700

    Update Spawner scripts

--- Assets/_Data/Spawner/Spawner.cs
+++ Assets/_Data/Spawner/Spawner.cs
@@ -5,19 +5,19 @@ using UnityEngine.Jobs;
 
 public abstract class Spawner : TienMonoBehaviour
 {
-    [SerializeField] protected List<Transform> prefabsList = new();
+    [SerializeField] protected List<Transform> prefabs = new();
     [SerializeField] protected List<Transform> poolObjs = new();
     [SerializeField] protected Transform holder;
     protected override void LoadComponents()
     {
         base.LoadComponents();
-        LoadPrefabsList();
+        LoadPrefabs();
         LoadHolder();
     }
 
     protected virtual Transform GetPrefabByName(string prefabName)
     {
-        foreach (Transform prefab in prefabsList)
+        foreach (Transform prefab in prefabs)
         {
             if (prefab == null) continue;
             if (prefab.name == prefabName) return prefab;
@@ -27,48 +27,54 @@ public abstract class Spawner : TienMonoBehaviour
 
     protected virtual Transform GetObjectFromPool(Transform prefab)
     {
-        Transform gameObject = GetPrefabByName(prefab.gameObject.name);
-        if (gameObject == null) return null;
-        foreach (Transform child in poolObjs)
+        foreach (Transform poolObj in poolObjs)
         {
-            if (child.name == prefab.name) return child;
+            if (poolObj.name == prefab.name) return poolObj;
         }
-        return null;
+
+        Transform newPrefab = Instantiate(prefab);
+        newPrefab.name = prefab.name;
+        return newPrefab;
     }
 
-    protected virtual void Spawn(string prefabName, Vector3 position)
+    protected virtual Transform Spawn(string prefabName, Vector3 position)
     {
         Transform prefab = GetPrefabByName(prefabName);
-        Transform gameObj = GetObjectFromPool(prefab);
-        if (gameObj == null)
+        if (prefab == null)
         {
-            gameObj = Instantiate(prefab).transform;
-            gameObj.SetLocalPositionAndRotation(position, Quaternion.identity);
-            gameObj.SetParent(holder);
-            gameObj.gameObject.SetActive(true);
+            Debug.LogWarning($"Prefab not found: {prefabName}");
+            return null;
         }
+
+        Transform newPrefab = GetObjectFromPool(prefab);
+        newPrefab.SetPositionAndRotation(position, Quaternion.identity);
+        newPrefab.SetParent(holder);
+        newPrefab.gameObject.SetActive(true);
+
+        return newPrefab;
     }
 
-    public virtual void Despawn(Transform gameObj)
+    public virtual void Despawn(Transform obj)
     {
-        poolObjs.Add(gameObj);
-        gameObj.gameObject.SetActive(false);
+        poolObjs.Add(obj);
+        obj.gameObject.SetActive(false);
     }
 
-    protected virtual void LoadPrefabsList()
+    protected virtual void LoadPrefabs()
     {
-        if (prefabsList.Count > 0) return;
+        if (this.prefabs.Count > 0) return;
         Transform prefabs = transform.Find("Prefabs");
         foreach (Transform child in prefabs)
         {
-            prefabsList.Add(child);
+            this.prefabs.Add(child);
         }
+        Debug.Log($"{transform.name}: LoadPrefabs", gameObject);
         HideAllPrefabs();
     }
 
     protected virtual void HideAllPrefabs()
     {
-        foreach (Transform prefab in prefabsList) prefab.gameObject.SetActive(false);
+        foreach (Transform prefab in prefabs) prefab.gameObject.SetActive(false);
     }
 
     protected virtual void LoadHolder()
